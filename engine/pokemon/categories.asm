9e4aab8b6a498d999c1fd9c3051c752f9ac53cbb
andore92 <andore92@gmail.com>
2025-04-09 23:12:32 -0400

    more fixes after splitting bytes

--- engine/pokemon/categories.asm
+++ engine/pokemon/categories.asm
@@ -1,23 +1,23 @@
 GetMoveCategoryName:
 	; Copy the category name of move b to wStringBuffer1.
 	
-		ld a, b
+		ld a, b          ; b = move id
 		dec a
 		ld bc, MOVE_LENGTH
-		ld hl, Moves + MOVE_CATEGORY ; <-- moved to the new category byte
+		ld hl, Moves + MOVE_CATEGORY ; get pointer to category
 		call AddNTimes
 		ld a, BANK(Moves)
-		call GetFarByte
+		call GetFarByte  ; now A = category (0 = PHY, 1 = SPC, 2 = STA)
 	
-		; a = PHYSICAL (0), SPECIAL (1), or STATUS (2)
 		ld hl, CategoryNames
 		ld e, a
 		ld d, 0
 		add hl, de
-		add hl, de
+		add hl, de       ; 2-byte pointer
 		ld a, [hli]
 		ld h, [hl]
 		ld l, a
+	
 		ld de, wStringBuffer1
 		ld bc, MOVE_NAME_LENGTH
 		jp CopyBytes
