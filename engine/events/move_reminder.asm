8e76b5e2822226aafb5f8d36a9f424823d8f63e0
andore92 <andore92@gmail.com>
2025-04-09 20:10:53 -0400

    Revert "Update move_reminder.asm"
    This reverts commit 15cf3658fe9903b6537aa552d9996bffe54e6167.

--- engine/events/move_reminder.asm
+++ engine/events/move_reminder.asm
@@ -313,48 +313,25 @@ ChooseMoveToLearn::
 
 .PrintDetails
 	ld hl, wStringBuffer1
-	ld bc, 16
+	ld bc, wStringBuffer2 - wStringBuffer1
 	ld a, " "
 	call ByteFill
-	
 	ld a, [wMenuSelection]
 	inc a
 	ret z
 	dec a
 	push de
-	
-	; Get full move type (with PSS category bits)
+	dec a
+
+IF DEF(PSS)
 	ld bc, MOVE_LENGTH
-	ld hl, Moves + MOVE_TYPE
+	ld hl, Moves + MOVE_CATEGORY
 	call AddNTimes
 	ld a, BANK(Moves)
 	call GetFarByte
-	ld [wTempByteValue], a
-	
-	ld b, a
-	and TYPE_MASK ; just the type index
-	ld [wTempSpecies], a
-	
-	ld a, b
-	and %11000000 ; get PSS bits
-	cp PHYSICAL
-	jr z, .physical
-	cp SPECIAL
-	jr z, .special
-	ld hl, .CategoryStatus
-	jr .got_category
-.physical
-	ld hl, .CategoryPhysical
-	jr .got_category
-.special
-	ld hl, .CategorySpecial
-.got_category
-	ld de, wStringBuffer1
-	ld bc, 3
-	call PlaceString
-	
-	; print type
-	ld a, [wTempSpecies]
+	and CATEGORY_MASK
+
+; bc = a * 4
 	add a
 	add a
 	ld b, 0
@@ -363,33 +340,66 @@ ChooseMoveToLearn::
 	add hl, bc
 	ld d, h
 	ld e, l
-	ld hl, wStringBuffer1 + 3
-	ld bc, 4
+	ld hl, wStringBuffer1
+	ld bc, 3
 	call PlaceString
-	
-	; separator
-	ld hl, wStringBuffer1 + 7
+	ld hl, wStringBuffer1 + 3
 	ld [hl], "/"
-	
-	; power
+
 	ld a, [wMenuSelection]
 	dec a
+ENDC
+
+	ld bc, MOVE_LENGTH
+	ld hl, Moves + MOVE_TYPE
+	call AddNTimes
+	ld a, BANK(Moves)
+	call GetFarByte
+    ld [wTempByteValue], a ; ld [wd265], a
+
+; bc = a * 4
+	add a
+	add a
+	ld b, 0
+	ld c, a
+	ld hl, .Types
+	add hl, bc
+	ld d, h
+	ld e, l
+	ld hl, wStringBuffer1 + 4
+	ld bc, 3
+	call PlaceString
+	ld hl, wStringBuffer1 + 7
+	ld [hl], "/"
+
+	ld a, [wMenuSelection]
+	dec a
+	
 	ld bc, MOVE_LENGTH
 	ld hl, Moves + MOVE_POWER
 	call AddNTimes
 	ld a, BANK(Moves)
 	call GetFarByte
-	ld [wTempByteValue], a
-	ld de, wTempByteValue
 	ld hl, wStringBuffer1 + 8
+	and a
+	jr z, .no_power
+    ld [wTempByteValue], a ; ld [wBuffer1], a
+	ld de, wTempByteValue ; ld de, wBuffer1
 	lb bc, 1, 3
 	call PrintNum
-	
-	; separator
+	jr .got_power
+.no_power
+	ld de, .ThreeDashes
+	ld bc, 3
+	call PlaceString
+.got_power
 	ld hl, wStringBuffer1 + 11
 	ld [hl], "/"
-	
-	; pp
+
+	ld a, [wMenuSelection]
+	dec a
+
+; Print PP (works)
 	ld a, [wMenuSelection]
 	dec a
 	ld bc, MOVE_LENGTH
@@ -397,23 +407,20 @@ ChooseMoveToLearn::
 	call AddNTimes
 	ld a, BANK(Moves)
 	call GetFarByte
-	ld [wTempByteValue], a
-	ld de, wTempByteValue
+    ld [wTempByteValue], a ; ld [wBuffer1], a
 	ld hl, wStringBuffer1 + 12
+    ld de, wTempByteValue ; ld de, wBuffer1
 	lb bc, 1, 2
 	call PrintNum
-	
-	; null-terminator
 	ld hl, wStringBuffer1 + 14
 	ld [hl], "@"
-	
+
 	pop hl
 	ld de, wStringBuffer1
 	jp PlaceString
 	
-.CategoryPhysical db "PHY"
-.CategorySpecial  db "SPC"
-.CategoryStatus   db "STA"
+.ThreeDashes
+	db "---@"
 	
 .Types
 	db "NRM@"
@@ -445,7 +452,6 @@ ChooseMoveToLearn::
 	db "DRG@"
 	db "DRK@"
 	db "FRY@"
-	
 
 .PrintMoveDesc
 	push de
