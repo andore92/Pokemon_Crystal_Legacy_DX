fd3e3fd13d51d9300992ef57cecc95001c4fe821
andore92 <andore92@gmail.com>
2025-04-09 22:25:40 -0400

    quick hunch

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -4,7 +4,8 @@
 ; - PokedexTypeSearchStrings (see data/types/search_strings.asm)
 ; - TypeMatchups (see data/types/type_matchups.asm)
 ; - TypeBoostItems (see data/types/type_boost_items.asm)
-	const_def
+const_def
+
 
 	const NORMAL
 	const FIGHTING
@@ -14,13 +15,14 @@
 	const ROCK
 	const BIRD
 	const BUG
-	const GHOST ; Ghost is made special whenever psysical/special checks are made, this preserves trade compatability
+	const GHOST
 	const STEEL
 
-UNUSED_TYPES EQU const_value
+DEF UNUSED_TYPES EQU const_value
 	const_next 19
 	const CURSE_TYPE
-UNUSED_TYPES_END EQU const_value
+DEF UNUSED_TYPES_END EQU const_value
+
 
 	const FIRE
 	const WATER
@@ -29,14 +31,14 @@ UNUSED_TYPES_END EQU const_value
 	const PSYCHIC_TYPE
 	const ICE
 	const DRAGON
-	const DARK ; Dark is made physical whenever psysical/special checks are made, this preserves trade compatability
-TYPES_END EQU const_value
+	const DARK
+DEF TYPES_END EQU const_value
 
-TYPE_MASK EQU %00111111
-PHYSICAL  EQU %01000000
-SPECIAL   EQU %10000000
-STATUS    EQU %11000000
+DEF TYPE_MASK EQU %00111111
+DEF PHYSICAL  EQU %01000000
+DEF SPECIAL   EQU %10000000
+DEF STATUS    EQU %11000000
 
-NUM_TYPES EQU TYPES_END + UNUSED_TYPES - UNUSED_TYPES_END - 1 ; discount BIRD
+DEF NUM_TYPES EQU TYPES_END + UNUSED_TYPES - UNUSED_TYPES_END - 1 ; discount BIRD
 
-POKEDEX_TYPE_STRING_LENGTH EQU 9
+DEF POKEDEX_TYPE_STRING_LENGTH EQU 9
